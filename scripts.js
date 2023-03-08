39b2429e9122bf277b38b6424f7fa70ca1b34e5d
Crispon500 <crispon500@gmail.com>
2023-03-08 16:02:06 -0600

    playRound function created and tested

--- scripts.js
+++ scripts.js
@@ -1,6 +1,59 @@
+//function to grab a random element from an array of "rock", "paper", and "scissors"
 function getComputerChoice() {
     const randomElement = ["rock","paper","scissors"];
     return randomElement[Math.floor(Math.random() * randomElement.length)];
 }
 
-console.log(getComputerChoice());
+//function to compare and declare the winner between the user and computer
+function playRound(playerSelection, computerSelection) {
+    let result = "";
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            if(computerSelection == "rock"){
+                result = "It's a tie";
+            } else if(computerSelection == "paper")
+            {
+                result = "How can you lose to a computer?";
+            } else if(computerSelection == "scissors")
+            {
+                result = "You won!";
+            } else{
+                result = "somhow you broke the computer";
+            }
+            break;
+        case "paper":
+            if(computerSelection == "rock"){
+                result = "You won!";
+            } else if(computerSelection == "paper")
+            {
+                result = "It's a tie";
+            } else if(computerSelection == "scissors")
+            {
+                result = "How can you lose to a computer?";
+            } else{
+                result = "somhow you broke the computer";
+            }
+            break;
+        case "scissors":
+        if(computerSelection == "rock"){
+            result = "How can you lose to a computer?";
+        } else if(computerSelection == "paper")
+        {
+            result = "You won!";
+        } else if(computerSelection == "scissors")
+        {
+            result = "It's a tie";
+        } else{
+            result = "somhow you broke the computer";
+        }
+        break;
+        default:
+            result = "Insert a valid option";
+            break;
+    }
+    return result;
+}
+
+const playerSelection ="scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
