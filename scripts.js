ef9f73ecc254ffde6f5095732127b9d442d66a6c
KJT <crispon500@gmail.com>
2023-03-11 19:30:38 -0600

    remove buttons on game end

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 let playerWinCount = 0;
 let computerWinCount = 0;
 
+//create actions for the button choices
 const castRock = document.querySelector('#rock');
 castRock.onclick = () => playRound("rock",getComputerChoice());
 const castPaper = document.querySelector("#paper");
 castPaper.onclick = () => playRound("paper",getComputerChoice());
 const castScissors = document.querySelector("#scissors")
 castScissors.onclick = () => playRound("scissors",getComputerChoice());
+
+//used for placing the reset button
+const buttonContainer = document.getElementById("button-container");
+
+//create a reset button
+const resetButton = document.createElement("button");
+resetButton.innerText = "Reset";
+
 
 //function to grab a random element from an array of "rock", "paper", and "scissors"
 function getComputerChoice() {
@@ -18,23 +27,31 @@ function getComputerChoice() {
 function checkStatus() {
     if(playerWinCount === 5){
         document.getElementById("player-score").innerText = "Player: "+ playerWinCount;
-        alert("You won the war")
-        resetScore();
-        return true;
+        removeCastButtons();
+        updateAnnouncementBoard("You won the game!!!");
     }
     else if(computerWinCount === 5){
         document.getElementById("computer-score").innerText = "Computer: " + computerWinCount;
-        alert("Why do you suck so much.")
-        resetScore();
-        return true;
-    }
-    else{
-        return false;
+        removeCastButtons();
+        updateAnnouncementBoard("Why do you suck so much. Looks like you lost for good.");
     }
 }
-function resetScore(){
+//this is to remove the Rock Paper Scissors buttons when the game is over and also adds the reset button
+function removeCastButtons(){
+    document.getElementById("rock").remove();
+    document.getElementById("paper").remove();
+    document.getElementById("scissors").remove();
+    buttonContainer.append(resetButton);
+    
+}
+
+function resetGame(){
     playerWinCount = 0;
     computerWinCount = 0;
+}
+
+function updateAnnouncementBoard(text){
+    document.getElementById("announcement-board").innerText = text;
 }
 
 //function to compare and declare the winner between the user and computer
@@ -51,7 +68,7 @@ function playRound(playerSelection, computerSelection) {
             } else if(computerSelection == "scissors")
             {
                 playerWinCount++;
-                result = "You won!";
+                result = "You win this round.";
             } else{
                 result = "somhow you broke the computer";
             }
@@ -59,7 +76,7 @@ function playRound(playerSelection, computerSelection) {
         case "paper":
             if(computerSelection == "rock"){
                 playerWinCount++;
-                result = "You won!";
+                result = "You win this round.";
             } else if(computerSelection == "paper")
             {
                 result = "It's a tie";
@@ -78,7 +95,7 @@ function playRound(playerSelection, computerSelection) {
         } else if(computerSelection == "paper")
         {
             playerWinCount++;
-            result = "You won!";
+            result = "You win this round.";
         } else if(computerSelection == "scissors")
         {
             result = "It's a tie";
@@ -90,7 +107,7 @@ function playRound(playerSelection, computerSelection) {
             result = "Insert a valid option";
             break;
     }
-    document.getElementById("announcement-board").innerText = result;
+    updateAnnouncementBoard(result);
     checkStatus();
     document.getElementById("player-score").innerText = "Player: "+ playerWinCount;
     document.getElementById("computer-score").innerText = "Computer: " + computerWinCount;
